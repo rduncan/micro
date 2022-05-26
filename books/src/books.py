eeec9f6222e8708133874ff45689fe084167daa6
Rich Duncan <rich@cuttingedge.io>
2022-05-25 20:46:21 -0400

    savepoint

--- books/src/books.py
+++ books/src/books.py
@@ -1,11 +1,12 @@
 import json
-from flask import Flask,jsonify
+from flask import Flask,request
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import Schema, fields
+from flask_restful import Resource, Api
+from marshmallow import Schema, fields, post_load
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/testdb'
-
+api = Api(app)
 db = SQLAlchemy(app)
 
 class Book(db.Model):
@@ -21,18 +22,24 @@ class BookSchema(Schema):
     id = fields.Integer()
     title = fields.String()
     author = fields.Integer()
+    @post_load
+    def make_book(self, data):
+        return Book(**data)
 
+class BookListResource(Resource):
+    def get(self):
+        books = Book.query.all()
+        schema = BookSchema()
+        return [ schema.dump(b) for b in books ]
 
-@app.route('/')
-def index():
-    return json.dumps({'name': 'alicex',
-                       'email': 'alice@outlook.com'})
+    def post(self):
+        book = BookSchema.load(request.get_json())
+        db.session.add(book)
+        db.session.commit()
+        schema = BookSchema()
+        return schema.dump(book)
 
-@app.route('/api/books', methods=['GET'])
-def get_all_books():
-    books = Book.query.all()
-    schema = BookSchema()
-    return jsonify( [ schema.dump(b) for b in books])
+api.add_resource(BookListResource, '/books')
 
 if __name__ == '__main__':
         script_name = __file__
